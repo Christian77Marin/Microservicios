07dd0acaed65a6fae2f942e8475005365a48812c
Christian Marin <christianmarinrevelles@gmail.com>
2023-01-17 00:01:33 +0100

    Initial Commit

---
+++ 02_microservicio_cursos/src/main/java/com/cursos/controllers/CursosController.java
@@ -0,0 +1,53 @@
+package com.cursos.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursos.model.Curso;
+
+import jakarta.annotation.PostConstruct;
+
+@RestController
+public class CursosController {
+	
+	private List<Curso> cursos;
+	@PostConstruct
+	public void init() {
+		cursos = new ArrayList<>();
+		cursos.add(new Curso("Spring", 100, "Tarde"));
+		cursos.add(new Curso("JavaScript", 60, "Mañana"));
+		cursos.add(new Curso("C#", 50, "Mañana"));
+		cursos.add(new Curso("GoLang", 25, "Tarde"));
+		cursos.add(new Curso("TypeScript", 300, "Mañana"));
+		cursos.add(new Curso("Angular", 200, "Mañana"));
+		cursos.add(new Curso("Tailwind", 20, "Tarde"));
+		cursos.add(new Curso("Spring", 23, "Mañana"));
+	}
+	
+	@GetMapping(value="cursos", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> getCursos(){
+		return cursos;
+	}
+	
+	@GetMapping(value="curso", produces = MediaType.APPLICATION_XML_VALUE) //--> Añadiendo la libreria jackson del pom e indicando el mediatype como xml se hace la conversion a xml automaticamente
+	public Curso getCurso() {
+		return new Curso("Java", 100, "mañana");
+	}
+	
+	@GetMapping(value="cursos/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> buscarCursos(@PathVariable("name") String nombre){
+		List<Curso> aux = new ArrayList<>();
+		for(Curso c: cursos) { // --> Recorre toda la lista 
+			if(c.getNombre().equals(nombre)) {
+				aux.add(c);
+			}
+		}
+		return aux;
+	}
+
+}
