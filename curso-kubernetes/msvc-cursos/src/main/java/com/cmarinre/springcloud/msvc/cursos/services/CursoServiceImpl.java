01bf1a75efcbf65bc119491af7ecadd45c705c3d
Christian Marin <christianmarinrevelles@gmail.com>
2023-06-29 19:42:37 +0200

    Update CursoServiceImpl.java

--- curso-kubernetes/msvc-cursos/src/main/java/com/cmarinre/springcloud/msvc/cursos/services/CursoServiceImpl.java
+++ curso-kubernetes/msvc-cursos/src/main/java/com/cmarinre/springcloud/msvc/cursos/services/CursoServiceImpl.java
@@ -3,6 +3,7 @@ package com.cmarinre.springcloud.msvc.cursos.services;
 import com.cmarinre.springcloud.msvc.cursos.clients.UsuarioClientRest;
 import com.cmarinre.springcloud.msvc.cursos.models.Usuario;
 import com.cmarinre.springcloud.msvc.cursos.models.entity.Curso;
+import com.cmarinre.springcloud.msvc.cursos.models.entity.CursoUsuario;
 import com.cmarinre.springcloud.msvc.cursos.repositories.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -46,16 +47,79 @@ public class CursoServiceImpl implements CursoService{
 
     @Override
     public Optional<Usuario> asignarUsuario(Usuario usuario, Long cursoId) {
+
+        //Curso
+        Optional<Curso> o = repo.findById(cursoId);
+        if(o.isPresent()){
+
+            //usuario recogido del servicio Usuarios
+            Usuario usuarioMsvc = client.findById(usuario.getId());
+
+            //Curso Recogido
+            Curso curso = o.get();
+
+            //Relacion entre curso y usuario
+            CursoUsuario cursoUsuario = new CursoUsuario();
+            cursoUsuario.setUsuarioId(usuarioMsvc.getId());
+
+            curso.addCursoUsuario(cursoUsuario);
+            repo.save(curso);
+
+            return Optional.of(usuarioMsvc);
+        }
+
         return Optional.empty();
+
     }
 
     @Override
     public Optional<Usuario> crearUsuario(Usuario usuario, Long cursoId) {
+
+        //Curso
+        Optional<Curso> o = repo.findById(cursoId);
+        if(o.isPresent()){
+
+            //usuario recogido del servicio Usuarios
+            Usuario usuarioNuevoMsvc = client.save(usuario);
+
+            //Curso Recogido
+            Curso curso = o.get();
+
+            //Relacion entre curso y usuario
+            CursoUsuario cursoUsuario = new CursoUsuario();
+            cursoUsuario.setUsuarioId(usuarioNuevoMsvc.getId());
+
+            curso.addCursoUsuario(cursoUsuario);
+            repo.save(curso);
+
+            return Optional.of(usuarioNuevoMsvc);
+        }
+
         return Optional.empty();
     }
 
     @Override
     public Optional<Usuario> desasignarUsuario(Usuario usuario, Long cursoId) {
+        //Curso
+        Optional<Curso> o = repo.findById(cursoId);
+        if(o.isPresent()){
+
+            //usuario recogido del servicio Usuarios
+            Usuario usuarioMsvc = client.findById(usuario.getId());
+
+            //Curso Recogido
+            Curso curso = o.get();
+
+            //Relacion entre curso y usuario
+            CursoUsuario cursoUsuario = new CursoUsuario();
+            cursoUsuario.setUsuarioId(usuarioMsvc.getId());
+
+            curso.removeCursoUsuario(cursoUsuario);
+            repo.save(curso);
+
+            return Optional.of(usuarioMsvc);
+        }
+
         return Optional.empty();
     }
 }
